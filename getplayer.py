35be7dfa9eab0fc097be5e2ce8feffdca150f0da
Tyler Walsh <tjwalsh@udel.edu>
2026-08-03 16:34:43 -0400

    Added team object to make it easier to retrieve some team information, like division and conference

--- getplayer.py
+++ getplayer.py
@@ -1,6 +1,7 @@
 import nba_api.stats.static.players as static_players
 import nba_api.stats.static.teams as static_teams
 import nba_api.stats.endpoints.commonplayerinfo as commonplayerinfo
+import nba_api.stats.endpoints.teaminfocommon as teaminfocommon
 import random
 import pandas as pd
 from datetime import datetime, date
@@ -13,11 +14,11 @@ def get_random_active_player():
     return player
 
 
-
 def get_player_id(player): 
     # returns ID of a player so we can retrieve their stats/info
     # helper function for get_player_info
     return player['id']
+
 
 def get_player_info(player):
     # takes a player dictionary and returns a pandas view of their information
@@ -25,6 +26,7 @@ def get_player_info(player):
     p_id = get_player_id(player)
     player = commonplayerinfo.CommonPlayerInfo(player_id=p_id).common_player_info.get_data_frame().iloc[0]
     return player
+
 
 def initialize_player():
     # initialize a random player.
@@ -34,10 +36,11 @@ def initialize_player():
     
 
 def get_age(player):
-    # takes a birthdate string and a datetime object and returns the age as a string
+    # takes a birthdate string and a datetime object and returns the age as an integer
     bday = player.loc['BIRTHDATE']
     today = datetime.now()
     bday_str = str(bday).strip()
+
     if "T" in bday_str:
         bday_str = bday_str.split("T", 1)[0]
 
@@ -45,6 +48,7 @@ def get_age(player):
     today_date = today.date() if isinstance(today, datetime) else today
 
     age = today_date.year - birth_date.year
+
     if (today_date.month, today_date.day) < (birth_date.month, birth_date.day):
         age -= 1
 
@@ -76,9 +80,47 @@ def format_height_in_feet_inches(total_inches):
     inches = total_inches % 12
     return f"{feet}-{inches}"
 
+# TEAM CLASS
+class Team:
 
+    def __init__(self, id):
+        # because we will only initialize a team object when we are setting up a player,
+        # we will initialize the team with the provided ID
+        try:
+            # attempt to get team info from api, id comes from player.set_info() 
+            # then generate team object
+            df = teaminfocommon.TeamInfoCommon(team_id=id).get_data_frames()
+            df = df[0]
+            df = df.iloc[0]
+            self.name = df.loc['TEAM_NAME']
+            self.conference = df.loc['TEAM_CONFERENCE']
+            self.division = df.loc['TEAM_DIVISION']
+            self.id = df.loc['TEAM_ID']
+            self.abbreviation = df.loc['TEAM_ABBREVIATION']
+        except Exception:
+            # exception if a player is not currently rostered on a team. 
+            # if a player is not rostered on a team i plan to just get another player. work around for now
+            static_team = next(
+                (team for team in static_teams.get_teams() if team.get('id') == id),
+                None,
+            )
+            if static_team is None:
+                self.name = "Unknown"
+                self.conference = None
+                self.division = None
+                self.id = id
+                self.abbreviation = "UNK"
+            else:
+                self.name = static_team['full_name']
+                self.conference = static_team.get('conference')
+                self.division = static_team.get('division')
+                self.id = static_team['id']
+                self.abbreviation = static_team['abbreviation']
+
+# PLAYER CLASS
 class Player:
     def __init__(self):
+        # initialize empty player
         self.name = None
         self.age = None
         self.position = None
@@ -88,7 +130,7 @@ class Player:
         self.id = None
 
     def __str__(self):
-        return f"Player: {self.name}, Age: {self.age}, Position: {self.position}, Height: {format_height_in_feet_inches(self.height)}, Team: {self.team}, Jersey: {self.jersey}"
+        return f"Player: {self.name}, Age: {self.age}, Position: {self.position}, Height: {format_height_in_feet_inches(self.height)}, Team: {self.team.abbreviation}, Jersey: {self.jersey}"
 
     def print(self):
         print(self)
@@ -103,37 +145,35 @@ class Player:
             print("Multiple players found with that name.")
         else:
             print("Player not found.")
-    
+
+
     def set_info(self, player):
-        # helper function to set info
+        # set player info, takes player dictionary and updates player object
+        # we will initialize team with team object that is passed a team ID
         self.id = player.loc['PERSON_ID']
         self.name = player.loc['DISPLAY_FIRST_LAST']
         self.age = get_age(player)
         self.position = player.loc['POSITION']
         self.height = get_height_in_inches(player.loc['HEIGHT'])
-        self.team = player.loc['TEAM_ABBREVIATION']
+        self.team = Team(player.loc['TEAM_ID'])
         self.jersey = player.loc['JERSEY']
-        if len(self.team) == 0:
+
+        if len(self.team.name) == 0: # if team name is not set, get a new player
+            self.set_random_player()
+        if len(self.jersey) == 0: # if no jersey number, get a new player
             self.set_random_player()
 
     def set_random_player(self):
         player = initialize_player()
         self.set_info(player)
 
-# def compare_players(p1, p2):
-#     diff = {}
-#     if p1.name != p2.name:
-#         diff['age'] = p1.age - p2.age
-        
-
-# def compare_players(p1, p2_):
-
+# comparison functions
 def compare_position(p1, p2):
+    # compare positions of two players, return string describing difference
+    # we will find the index of player position and use difference to find distance
     positions = ["Guard", "Guard-Forward", "Forward-Guard", "Forward", "Forward-Center", "Center-Forward", "Center"]
     pos1 = positions.index(p1.position)
-    print(pos1)
     pos2 = positions.index(p2.position)
-    print(pos2)
     diff = pos1 - pos2
     if diff == 0:
         return "position ="
@@ -145,6 +185,7 @@ def compare_position(p1, p2):
 def compare_height(p1, p2):
     h1 = p1.height
     h2 = p2.height
+
     diff = h1 - h2
     if diff == 0:
         return "height ="
@@ -160,6 +201,7 @@ def compare_height(p1, p2):
 def compare_age(p1, p2):
     a1 = p1.age
     a2 = p2.age
+
     diff = a1 - a2
     if diff == 0:
         return "age ="
@@ -175,6 +217,13 @@ def compare_age(p1, p2):
 def compare_jersey(p1, p2):
     j1 = int(p1.jersey)
     j2 = int(p2.jersey)
+
+    # edge case for a player's jersey is "00"
+    if p1.jersey == "00":
+        j1 = -1
+    if p2.jersey == "00":
+        j2 = -1
+
     diff = j1 - j2
     if diff == 0:
         return "jersey ="
@@ -190,11 +239,11 @@ def compare_jersey(p1, p2):
 
 if __name__ == "__main__":
     player = Player()
-    player.set_random_player()
+    player.set_player_by_name("Jayson Tatum")
     player.print()
 
     player2 = Player()
-    player2.set_random_player()
+    player2.set_player_by_name("Jordan Clarkson")
     player2.print()
 
     print(compare_position(player, player2))
--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 import nba_api.stats.static.players as static_players
 import nba_api.stats.static.teams as static_teams
 import nba_api.stats.endpoints.commonplayerinfo as commonplayerinfo
+import nba_api.stats.endpoints.teaminfocommon as teaminfocommon
+import pandas as pd
 
 
 teams = static_teams.get_teams()
-team_abrv = []
+team_dict = {}
 for team in teams:
-    team_abrv.append(team['abbreviation'])
-
-print(team_abrv)
+    team_dict[team['abbreviation']] = team['id']
+# print(team_dict)
+df = teaminfocommon.TeamInfoCommon(team_id=team_dict['LAL']).get_data_frames()
+df = df[0]
+df = df.iloc[0]
+print(df)
+# print(teams)
+# print(team_abrv)
 
 def main():
     pass
